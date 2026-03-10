fa677fd2074c5fd6e14a878146274515620903d8
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-03-10 10:03:31 +0530

    Create 219-ContainsDuplicateIi

---
+++ 219-ContainsDuplicateIi/219-ContainsDuplicateIi.js
@@ -0,0 +1,22 @@
+// Last updated: 10/03/2026, 10:03:29
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+
+    for(let i = 0; i < nums.length; i++){
+
+        for(let j = i + 1; j <= i + k && j < nums.length; j++){
+
+            if(nums[i] === nums[j]){
+                return true;
+            }
+
+        }
+
+    }
+
+    return false;
+};
