fc64349498dca4ad6cc70124e4c482668a6c5153
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-03-10 10:03:15 +0530

    Create 442-FindAllDuplicatesInAnArray

---
+++ 442-FindAllDuplicatesInAnArray/442-FindAllDuplicatesInAnArray.js
@@ -0,0 +1,21 @@
+// Last updated: 10/03/2026, 10:03:14
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function(nums) {
+    let result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        let index = Math.abs(nums[i]) - 1;
+
+        if (nums[index] < 0) {
+            result.push(Math.abs(nums[i]));
+        } else {
+            nums[index] = -nums[index];
+        }
+    }
+
+    return result;
+};
+
