3fd8a24a41095e74cee451f98c8853f5b7db2e77
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-03-10 10:03:48 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.js
@@ -0,0 +1,16 @@
+// Last updated: 10/03/2026, 10:03:47
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    for(let i=digits.length-1;i>=0;i--){
+        if(digits[i]<9){
+            digits[i]++;
+            return digits
+        }
+        digits[i]=0;
+    }
+    digits.unshift(1);
+    return digits;
+};
