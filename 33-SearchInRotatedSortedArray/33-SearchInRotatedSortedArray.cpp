814d359fb02624d963220190cbfec035ab344d6a
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-03-10 10:03:55 +0530

    Create 33-SearchInRotatedSortedArray

---
+++ 33-SearchInRotatedSortedArray/33-SearchInRotatedSortedArray.cpp
@@ -0,0 +1,12 @@
+// Last updated: 10/03/2026, 10:03:53
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
