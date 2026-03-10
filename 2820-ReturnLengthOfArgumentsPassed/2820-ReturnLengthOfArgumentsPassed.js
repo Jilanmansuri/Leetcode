3ddd84b0de8a2da4c8ff5c9a3b66179b3c1d91e6
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-03-10 10:02:44 +0530

    Create 2820-ReturnLengthOfArgumentsPassed

---
+++ 2820-ReturnLengthOfArgumentsPassed/2820-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,12 @@
+// Last updated: 10/03/2026, 10:02:42
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
