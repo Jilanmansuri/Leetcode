0151957bc33a1fac5c0a2f59c80baf8542ebac6d
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-03-10 10:03:45 +0530

    Create 122-BestTimeToBuyAndSellStockIi

---
+++ 122-BestTimeToBuyAndSellStockIi/122-BestTimeToBuyAndSellStockIi.js
@@ -0,0 +1,17 @@
+// Last updated: 10/03/2026, 10:03:43
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let profit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1]) {
+            profit += prices[i] - prices[i - 1];
+        }
+    }
+
+    return profit;
+};
+
