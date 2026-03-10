1a762a680d09486813fc19c5432528cc4b18dab0
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-03-10 10:02:54 +0530

    Create 2232-AddingSpacesToAString

---
+++ 2232-AddingSpacesToAString/2232-AddingSpacesToAString.js
@@ -0,0 +1,20 @@
+// Last updated: 10/03/2026, 10:02:53
+/**
+ * @param {string} s
+ * @param {number[]} spaces
+ * @return {string}
+ */
+var addSpaces = function(s, spaces) {
+    let j=0;
+    let result="";
+    for(let i=0;i<s.length;i++){
+        if(j<spaces.length && i == spaces[j]){
+           result += " ";
+           j++;
+        }
+        result+=s[i];
+    }
+
+
+    return result;
+};
