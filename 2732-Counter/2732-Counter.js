d8c295f1f03fece80e498157f808184b7015b907
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-03-10 10:02:48 +0530

    Create 2732-Counter

---
+++ 2732-Counter/2732-Counter.js
@@ -0,0 +1,18 @@
+// Last updated: 10/03/2026, 10:02:47
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
