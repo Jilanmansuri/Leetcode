94e4a5dd93ff6446678ae3b9236758e0b31cb702
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-03-12 17:18:50 +0530

    Create 709-ToLowerCase

---
+++ 709-ToLowerCase/version/javascript/709-ToLowerCase_20260312_171847.js
@@ -0,0 +1,14 @@
+// Last updated: 12/03/2026, 17:18:47
+/*
+ * 0ms
+ * 53.69MB
+ * Javascript
+*/
+
+1/**
+2 * @param {string} s
+3 * @return {string}
+4 */
+5var toLowerCase = function(s) {
+6    return s.toLowerCase();
+7};
