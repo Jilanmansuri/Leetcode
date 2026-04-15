bc2f9e4c0edda1a30c7e966ea8dfbabab9ee7729
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-04-15 10:08:19 +0530

    Create 205-IsomorphicStrings

---
+++ 205-IsomorphicStrings/version/cpp/205-IsomorphicStrings_20260415_100817.cpp
@@ -0,0 +1,30 @@
+// Last updated: 15/04/2026, 10:08:17
+// Gpt
+1class Solution {
+2public:
+3    bool isIsomorphic(string s, string t) {
+4        unordered_map<char,char>mp1;
+5        unordered_map<char,char>mp2;
+6        for(int i=0;i<s.size();i++){
+7            char c1=s[i];
+8            char c2=t[i];
+9            if(mp1.count(c1)){
+10                if(mp1[c1] != c2)
+11                return false;
+12            }
+13            else{
+14                mp1[c1]=c2;
+15            }
+16
+17            if(mp2.count(c2)){
+18                if(mp2[c2] != c1)
+19                return false;
+20            }
+21
+22            else{
+23                mp2[c2]=c1;
+24            }
+25        }
+26        return true;
+27    }
+28};
