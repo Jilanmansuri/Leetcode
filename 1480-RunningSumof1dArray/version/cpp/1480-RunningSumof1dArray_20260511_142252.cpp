509dae5fcee830c1c58170b45c189a27909e7f48
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-05-11 14:22:52 +0530

    Create 1480-RunningSumof1dArray

---
+++ 1480-RunningSumof1dArray/version/cpp/1480-RunningSumof1dArray_20260511_142252.cpp
@@ -0,0 +1,14 @@
+// Last updated: 11/05/2026, 14:22:52
+1class Solution {
+2public:
+3    vector<int> runningSum(vector<int>& nums) {
+4       
+5           vector<int>arr;
+6            int sum = 0;
+7            for (int i = 0; i < nums.size(); i++) {
+8                sum = sum + nums[i] ;
+9                arr.push_back(sum);
+10            }
+11            return arr;
+12    }
+13};
