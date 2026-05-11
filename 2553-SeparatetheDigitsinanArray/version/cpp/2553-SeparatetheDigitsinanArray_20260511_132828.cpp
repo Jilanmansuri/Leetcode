727f70046cad582fc3bf7203ba7bf6807fd40046
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-05-11 13:28:29 +0530

    Create 2553-SeparatetheDigitsinanArray

---
+++ 2553-SeparatetheDigitsinanArray/version/cpp/2553-SeparatetheDigitsinanArray_20260511_132828.cpp
@@ -0,0 +1,24 @@
+// Last updated: 11/05/2026, 13:28:28
+1class Solution {
+2public:
+3    vector<int> separateDigits(vector<int>& nums) {
+4
+5        vector<int> res;
+6
+7        for(int num : nums) {
+8
+9            int start = res.size();
+10
+11            while(num > 0) {
+12
+13                int digit = num % 10;
+14
+15                res.insert(res.begin() + start, digit);
+16
+17                num /= 10;
+18            }
+19        }
+20
+21        return res;
+22    }
+23};
