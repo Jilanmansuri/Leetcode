088a2a14944c31769e67d1bf924142ed21aa7632
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-05-13 13:04:53 +0530

    Create

---
+++ problem-1778657693028/version/cpp/problem-1778657693028_20260513_130453.cpp
@@ -0,0 +1,12 @@
+// Last updated: 13/05/2026, 13:04:53
+1class Solution {
+2public:
+3    int searchInsert(vector<int>& nums, int target) {
+4        for(int i=0;i<nums.size();i++){   
+5            if(nums[i]>=target){
+6                return i;
+7            }
+8        }
+9        return nums.size();
+10    }
+11};
