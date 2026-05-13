e27737b54622552d06e69c9491f85c635b89ef30
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-05-13 13:10:09 +0530

    Create 1319-UniqueNumberOfOccurrences

---
+++ 1319-UniqueNumberOfOccurrences/1319-UniqueNumberOfOccurrences.cpp
@@ -0,0 +1,18 @@
+// Last updated: 13/05/2026, 13:10:09
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> freq;
+        for (int num : arr) {
+            freq[num]++;
+        }
+        unordered_set<int> s;
+        for (auto it : freq) {
+            if (s.count(it.second)) {
+                return false;
+            }
+            s.insert(it.second);
+        }
+        return true;
+    }
+};
