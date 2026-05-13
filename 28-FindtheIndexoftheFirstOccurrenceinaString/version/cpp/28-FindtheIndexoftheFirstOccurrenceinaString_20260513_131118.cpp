1351354976e61feb06fb1f00d8949ed8e5d00be2
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-05-13 13:11:19 +0530

    Create 28-FindtheIndexoftheFirstOccurrenceinaString

---
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/version/cpp/28-FindtheIndexoftheFirstOccurrenceinaString_20260513_131118.cpp
@@ -0,0 +1,8 @@
+// Last updated: 13/05/2026, 13:11:18
+1class Solution {
+2public:
+3    int strStr(string haystack, string needle) {
+4        int index=haystack.find(needle);
+5        return index;
+6    }
+7};
