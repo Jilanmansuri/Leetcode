8d7fd5431c6ee16a008ab3b5f1c22b9ef0217395
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-05-13 13:29:09 +0530

    Create 961-N-RepeatedElementinSize2NArray

---
+++ 961-N-RepeatedElementinSize2NArray/version/cpp/961-N-RepeatedElementinSize2NArray_20260513_132907.cpp
@@ -0,0 +1,14 @@
+// Last updated: 13/05/2026, 13:29:07
+1class Solution {
+2public:
+3    int repeatedNTimes(vector<int>& nums) {
+4        for(int i=0;i<nums.size();i++){
+5            for(int j=i+1;j<nums.size();j++){
+6                if(nums[i]== nums[j]){
+7                    return nums[i];
+8                }
+9            }
+10        }
+11      return -1;
+12    }
+13};
