82ab38d5f1824f34bf6829fe1f82b43f5d99b7cb
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-05-19 20:28:24 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/version/cpp/263-UglyNumber_20260519_202821.cpp
@@ -0,0 +1,9 @@
+// Last updated: 19/05/2026, 20:28:21
+1class Solution {
+2public:
+3    vector<double> convertTemperature(double c){
+4        return{
+5            c+273.15,c*1.80+32.00
+6     };
+7    }
+8};
