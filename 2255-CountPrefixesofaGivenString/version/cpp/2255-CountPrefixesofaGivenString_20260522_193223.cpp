302a85eea46ce10991bf6e0a53a7fcb0e1472e48
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-05-22 19:32:23 +0530

    Create 2255-CountPrefixesofaGivenString

---
+++ 2255-CountPrefixesofaGivenString/version/cpp/2255-CountPrefixesofaGivenString_20260522_193223.cpp
@@ -0,0 +1,13 @@
+// Last updated: 22/05/2026, 19:32:23
+1class Solution {
+2public:
+3    int countPrefixes(vector<string>& words, string s) {
+4        int count=0;
+5        for(string word:words){
+6            if(s.find(word)== 0){
+7                count++;
+8            }
+9        }
+10        return count;
+11    }
+12};
