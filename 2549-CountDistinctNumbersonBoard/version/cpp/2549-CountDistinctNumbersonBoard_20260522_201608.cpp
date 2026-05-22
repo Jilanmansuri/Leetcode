869f15109eb229c721ec57e47bfded10da4abe9c
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-05-22 20:16:08 +0530

    Create 2549-CountDistinctNumbersonBoard

---
+++ 2549-CountDistinctNumbersonBoard/version/cpp/2549-CountDistinctNumbersonBoard_20260522_201608.cpp
@@ -0,0 +1,10 @@
+// Last updated: 22/05/2026, 20:16:08
+1class Solution {
+2public:
+3    int distinctIntegers(int n) {
+4        if(n == 1)
+5    return 1;
+6    
+7        return n-1;
+8    }
+9};
