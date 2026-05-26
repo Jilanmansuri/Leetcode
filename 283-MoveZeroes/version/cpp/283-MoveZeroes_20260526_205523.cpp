3239ff8fa968f820e943be07f3a303c3023dd4d3
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-05-26 20:55:25 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/version/cpp/283-MoveZeroes_20260526_205523.cpp
@@ -0,0 +1,13 @@
+// Last updated: 26/05/2026, 20:55:23
+1class Solution {
+2public:
+3    void moveZeroes(vector<int>& nums) {
+4        int j=0;
+5        for(int i=0;i<nums.size();i++){
+6            if(nums[i]!=0){
+7                swap(nums[i],nums[j]);
+8                j++;
+9            }
+10        }
+11    }
+12};
