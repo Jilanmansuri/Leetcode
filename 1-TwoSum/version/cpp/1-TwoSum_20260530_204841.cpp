9ee5bdff29f32692b1393604befe481c101e3ba4
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-05-30 20:48:42 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/version/cpp/1-TwoSum_20260530_204841.cpp
@@ -0,0 +1,18 @@
+// Last updated: 30/05/2026, 20:48:41
+1class Solution {
+2public:
+3    vector<int> twoSum(vector<int>& nums, int target) {
+4        unordered_map<int,int> mp;
+5        for(int i=0;i<nums.size();i++){
+6
+7            int complement = target-nums[i];
+8
+9            if(mp.find(complement)!=mp.end()){
+10                return {mp[complement],i};
+11
+12            }
+13            mp[nums[i]]=i;
+14        }
+15        return {};
+16    }
+17};
