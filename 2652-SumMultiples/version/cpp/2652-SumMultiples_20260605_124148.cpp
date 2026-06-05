0eef22bfc27b071b19ee233b210791291f5104d2
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-06-05 12:41:49 +0530

    Create 2652-SumMultiples

---
+++ 2652-SumMultiples/version/cpp/2652-SumMultiples_20260605_124148.cpp
@@ -0,0 +1,12 @@
+// Last updated: 05/06/2026, 12:41:48
+1class Solution {
+2public:
+3    string replaceDigits(string s) {
+4        for(int i=1;i<s.size();i+=2){
+5            s[i]=s[i-1]+(s[i] - '0');
+6                    //      1 -0 =1
+7                    //   a +1=b   
+8        }
+9        return s;
+10    }
+11};
