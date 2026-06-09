e46a4b21f2b612fa333790afe4336962842c2c57
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-06-09 16:42:35 +0530

    Create 2942-FindWordsContainingCharacter

---
+++ 2942-FindWordsContainingCharacter/version/cpp/2942-FindWordsContainingCharacter_20260609_164234.cpp
@@ -0,0 +1,16 @@
+// Last updated: 09/06/2026, 16:42:34
+1class Solution {
+2public:
+3    vector<int> findWordsContaining(vector<string>& words, char x) {
+4        vector<int>ans;
+5        for(int i=0;i<words.size();i++){
+6            for(char ch:words[i]){
+7                 if(ch==x){
+8                    ans.push_back(i);
+9                    break;
+10                 }
+11            }
+12        }
+13        return ans;
+14    }
+15};
