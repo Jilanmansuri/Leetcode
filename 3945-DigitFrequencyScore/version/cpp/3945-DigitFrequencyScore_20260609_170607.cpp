7bc604819bc675f0e85f96e5d6c466de27f8a4d6
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-06-09 17:06:08 +0530

    Create 3945-DigitFrequencyScore

---
+++ 3945-DigitFrequencyScore/version/cpp/3945-DigitFrequencyScore_20260609_170607.cpp
@@ -0,0 +1,18 @@
+// Last updated: 09/06/2026, 17:06:07
+1class Solution {
+2public:
+3    int digitFrequencyScore(int n) {
+4        int freq[10]={};
+5        while(n>0){
+6            int digit=n%10;
+7            freq[digit]++;
+8            n/=10;
+9        }
+10        int score=0;
+11
+12        for(int i=0;i<10;i++){
+13            score += i*freq[i];
+14        }
+15    return score;
+16    }
+17};
