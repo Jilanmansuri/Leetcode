038a435a476c439aa05357818a67c28af71e70c2
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-07-13 16:01:34 +0530

    Create 1221-SplitaStringinBalancedStrings

---
+++ 1221-SplitaStringinBalancedStrings/version/cpp/1221-SplitaStringinBalancedStrings_20260713_160132.cpp
@@ -0,0 +1,21 @@
+// Last updated: 13/07/2026, 16:01:32
+1class Solution {
+2public:
+3    int balancedStringSplit(string s) {
+4        int balance=0;
+5        int count=0;
+6        for(char ch:s){
+7            if(balance==0){
+8                count++;
+9            }
+10            if(ch=='R'){
+11                balance--;
+12            }
+13            if(ch=='L'){
+14                balance++;
+15            }
+16
+17        }
+18        return count;
+19    }
+20};
