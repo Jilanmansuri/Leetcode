a7fe5a42fefc847ffc773830328c9a44d773ffba
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-07-26 14:40:50 +0530

    Create 2089-FindTargetIndicesAfterSortingArray

---
+++ 2089-FindTargetIndicesAfterSortingArray/version/cpp/2089-FindTargetIndicesAfterSortingArray_20260726_144045.cpp
@@ -0,0 +1,20 @@
+// Last updated: 26/07/2026, 14:40:45
+1class Solution {
+2public:
+3    vector<int> targetIndices(vector<int>& nums, int target) {
+4        vector<int> ans;
+5        int smaller = 0;
+6        int equal = 0;
+7        for (int i = 0; i < nums.size(); i++) {
+8            if (nums[i] < target) {
+9                smaller++;
+10            } else if (nums[i] == target) {
+11                equal++;
+12            }
+13        }
+14        for (int i = smaller; i <smaller + equal; i++) {
+15            ans.push_back(i);
+16        }
+17        return ans;
+18    }
+19};
