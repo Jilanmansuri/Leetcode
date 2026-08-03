7c52d7b6c3b091e3c0e63b0f60f901bca033a035
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-08-03 10:11:10 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.cpp
@@ -0,0 +1,41 @@
+// Last updated: 03/08/2026, 10:11:06
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if (num == 1) {
+            return false;
+        }
+        int sum=0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum+=i;
+            }
+        }
+        return sum==num;
+    }
+};
+
+
+
+
+
+
+// class Solution {
+// public:
+//     bool checkPerfectNumber(int num) {
+//         if (num == 1) {
+//             return false;
+//         }
+//         int sum=1;
+//         for (int i = 2; i*i <= num; i++) {
+//             if (num % i == 0) {
+//                 sum+=i;
+
+//                 if(i!=num/i){
+//                     sum+=num/i;
+//                 }
+//             }
+//         }
+//         return sum==num;
+//     }
+// };
