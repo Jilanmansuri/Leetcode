2d0fcf6b20cafbdb00914bbb090b3a6e7532de93
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-08-03 10:10:42 +0530

    Create 748-LargestNumberAtLeastTwiceOfOthers

---
+++ 748-LargestNumberAtLeastTwiceOfOthers/748-LargestNumberAtLeastTwiceOfOthers.cpp
@@ -0,0 +1,18 @@
+// Last updated: 03/08/2026, 10:10:38
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums) {
+         int maxindex=0;
+         for(int i=0;i<nums.size();i++){
+            if(nums[i]> nums[maxindex]){
+                maxindex=i;
+            }
+         }
+         for(int i=0;i<nums.size();i++){
+            if(i!=maxindex && nums[maxindex] < 2*nums[i]){
+                return -1;
+            }
+         }
+         return maxindex;
+    }
+};
