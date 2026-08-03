28028f91f1b6406efb38633f4e99c2398a2903d1
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-08-03 10:11:50 +0530

    Create 392-IsSubsequence

---
+++ 392-IsSubsequence/392-IsSubsequence.cpp
@@ -0,0 +1,15 @@
+// Last updated: 03/08/2026, 10:11:46
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0;
+        int j=0;
+        while(i<s.size() && j<t.size()){
+            if(s[i]==t[j]){
+                i++;
+            }
+            j++;
+        }
+        return i== s.size();
+    }
+};
