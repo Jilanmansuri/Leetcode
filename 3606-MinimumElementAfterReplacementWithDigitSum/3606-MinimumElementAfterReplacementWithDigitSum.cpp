3ea928942ab7c8b4d8db779919dc2297e9f9707a
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-08-03 10:05:56 +0530

    Create 3606-MinimumElementAfterReplacementWithDigitSum

---
+++ 3606-MinimumElementAfterReplacementWithDigitSum/3606-MinimumElementAfterReplacementWithDigitSum.cpp
@@ -0,0 +1,20 @@
+// Last updated: 03/08/2026, 10:05:52
+class Solution {
+public:
+    int minElement(vector<int>& nums) {
+        int ans = INT_MAX;
+
+        for (int num : nums) {
+            int sum = 0;
+
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            ans = min(ans, sum);
+        }
+
+        return ans;
+    }
+};
