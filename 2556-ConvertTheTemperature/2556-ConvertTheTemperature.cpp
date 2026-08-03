bccfa707f40e40b936605b5c3716de320759024b
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-08-03 10:07:17 +0530

    Create 2556-ConvertTheTemperature

---
+++ 2556-ConvertTheTemperature/2556-ConvertTheTemperature.cpp
@@ -0,0 +1,9 @@
+// Last updated: 03/08/2026, 10:07:13
+class Solution {
+public:
+    vector<double> convertTemperature(double c){
+        return{
+            c+273.15,c*1.80+32.00
+     };
+    }
+};
