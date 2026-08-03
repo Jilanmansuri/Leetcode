5bb2af052268bf1d1a3582a7ceee478f81d35745
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-08-03 10:13:13 +0530

    Create 154-FindMinimumInRotatedSortedArrayIi

---
+++ 154-FindMinimumInRotatedSortedArrayIi/154-FindMinimumInRotatedSortedArrayIi.cpp
@@ -0,0 +1,15 @@
+// Last updated: 03/08/2026, 10:13:09
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int mn = nums[0];
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] < mn) {
+                mn = nums[i];
+            }
+        }
+
+        return mn;
+    }
+};
