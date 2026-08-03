b3d061f986fb10d3f645ed7a6bde59beff13a08a
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-08-03 10:13:46 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.cpp
@@ -0,0 +1,24 @@
+// Last updated: 03/08/2026, 10:13:42
+class Solution {
+public:
+
+    vector<int> plusOne(vector<int>& digits) {
+
+       
+        for(int i = digits.size() - 1; i >= 0; i--){
+
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+
+         
+            digits[i] = 0;
+        }
+
+     
+        digits.insert(digits.begin(), 1);
+
+        return digits;
+    }
+};
