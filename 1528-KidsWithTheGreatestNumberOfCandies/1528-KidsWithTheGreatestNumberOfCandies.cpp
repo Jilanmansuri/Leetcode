4da2a8886283add1830b41db3b6939d4398674e6
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-08-03 10:09:36 +0530

    Create 1528-KidsWithTheGreatestNumberOfCandies

---
+++ 1528-KidsWithTheGreatestNumberOfCandies/1528-KidsWithTheGreatestNumberOfCandies.cpp
@@ -0,0 +1,26 @@
+// Last updated: 03/08/2026, 10:09:32
+class Solution {
+public:
+
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+
+       
+        int maxi = *max_element(candies.begin(), candies.end());
+
+        vector<bool> ans;
+
+       
+        for(int candy : candies){
+
+          
+            if(candy + extraCandies >= maxi){
+                ans.push_back(true);
+            }
+            else{
+                ans.push_back(false);
+            }
+        }
+
+        return ans;
+    }
+};
