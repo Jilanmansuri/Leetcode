75120a2b5e4448d72262d1672dff6eb62764e17f
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-08-13 17:56:34 +0530

    Time: 3 ms (43.45%), Space: 8.3 MB (50.97%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string ans = "";
+
+        for (int i = 0; i < command.length(); i++) {
+
+            if (command[i] == '(' && command[i + 1] == ')') {
+                ans += 'o';
+                i++;
+            }
+            else if (command[i] == '(' || command[i] == ')') {
+                continue;
+            }
+            else {
+                ans += command[i];
+            }
+        }
+
+        return ans;
+    }
+};
