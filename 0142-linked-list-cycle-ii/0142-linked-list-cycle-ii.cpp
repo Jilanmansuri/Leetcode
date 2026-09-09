397b5005876074c2954ac73717bbd03e180086d4
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-09-09 09:21:50 +0530

    Time: 11 ms (14.75%), Space: 13.4 MB (10.58%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -5,29 +5,55 @@
  *     ListNode *next;
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
- */
+//  */
+
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        ListNode* slow=head;
-        ListNode* fast=head;
+        
+        ListNode *i = head;
+        unordered_set<ListNode*> s;
 
-        while (fast != NULL && fast->next != NULL) {
-            slow = slow->next;
-            fast = fast->next->next;
-
-            if (slow == fast) {
-
-                slow = head;
-
-                while (slow != fast) {
-                    slow = slow->next;
-                    fast = fast->next;
-                }
-
-                return slow;
+        while(i!=NULL){
+            if(s.find(i) != s.end()){
+                return i;
+            }else{
+                s.insert(i);
             }
+            i=i->next;
         }
-        return NULL;
+        return i;
     }
 };
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+//         ListNode* slow=head;
+//         ListNode* fast=head;
+
+//         while (fast != NULL && fast->next != NULL) {
+//             slow = slow->next;
+//             fast = fast->next->next;
+
+//             if (slow == fast) {
+
+//                 slow = head;
+
+//                 while (slow != fast) {
+//                     slow = slow->next;
+//                     fast = fast->next;
+//                 }
+
+//                 return slow;
+//             }
+//         }
+//         return NULL;
+//     }
+// };
