fb0626a342c4fd22b6f441f3338589337d72a148
Jilan Mansuri <jilan.f.mansuri.cg@gmail.com>
2026-09-15 16:29:47 +0530

    Time: 35 ms (33.36%), Space: 25.3 MB (24.91%) - LeetHub

---
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
@@ -0,0 +1,12 @@
+class Solution { 
+public: 
+    int maxDistinct(string s) { 
+        map<char, int> mp;
+
+        for(char ch : s) {
+            mp[ch]++;
+        }
+
+        return mp.size();
+    } 
+};
